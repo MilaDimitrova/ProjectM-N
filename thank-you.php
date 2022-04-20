df1e0cbbb17480461adea900f56750da2a889d7a
Mila Yonkova <miladimitrova11@gmail.com>
2022-04-20 20:26:56 +0300

    new

---
+++ thank-you.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thanks for Contact US - PHP MySQL</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="page-header">
+                <h2>Thanks for contacting us.</h2>
+                <h2>If you want to go back to our website, press here -> <a href="index.html">Home</a></h2>
+            </div>
+        </div>
+    </div>        
+</div>
+
+</body>
+</html>
