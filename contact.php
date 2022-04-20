80359203e5abd1f5c8b9022b4d67cf544b6b7ea1
Mila Yonkova <miladimitrova11@gmail.com>
2022-04-20 19:55:18 +0300

    project
    1st steps

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'contact';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS,  $DATABASE_NAME);
+
+if(mysqli_connect_error()){
+    exit('Error connecting to the database' . mysqli_connect_error());
+}
+
+if(!isset($_POST['username'],  $_POST['email'])){
+    exit('Empty Field(s)');
+}
+
+if(empty($_POST['username'] || empty($_POST['email']))){
+    exit('Values Empty');
+}
+
+if($stmt = $con->prepare('SELECT id  FROM questions WHERE username = ?')){
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if($stmt-> num_rows > 0){
+        echo 'Username already exist! Please try again!';
+    }else{
+        if($stmt = $con->prepare('INSERT INTO questions (username, email) VALUES (?, ?)')){
+            $stmt->bind_param('sss', $_POST['username'], $_POST['email']);
+            $stmt->execute();
+            echo 'Succesfully registered';
+            echo '<br>';
+            echo '<a href="index.html"> Now, you can look our website! :)';
+        }else{
+            echo 'Error Occurred';
+        }
+    }
+    $stmt->close();
+}else{
+        echo 'Error Occurred';
+}
+$con->close();
+?>
